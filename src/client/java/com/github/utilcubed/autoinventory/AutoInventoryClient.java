c324acaaa8ca9cee75402a15e7acd87ae1705b98
Boden-C <70297682+Boden-C@users.noreply.github.com>
2024-03-28 15:09:21 -0500

    Added more swap features

--- src/client/java/com/github/utilcubed/autoinventory/AutoInventoryClient.java
+++ src/client/java/com/github/utilcubed/autoinventory/AutoInventoryClient.java
@@ -23,34 +23,66 @@ public class AutoInventoryClient implements ClientModInitializer {
 			"key.autoinventory.opencontainer", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_Z,
 			"category.autoinventory.keybindings"));
 
-	private boolean wasAttacking = false;
-	private boolean wasUsing = false;
-	private boolean wasOpeningContainer = false;
+	public boolean wasAttacking = false;
+	public boolean wasUsing = false;
+	public boolean wasOpeningContainer = false;
+	public boolean wasFlying = false;
 
 	@Override
 	public void onInitializeClient() {
 
-		ClientTickEvents.END_CLIENT_TICK.register(client -> {
+		ClientTickEvents.START_CLIENT_TICK.register(client -> {
 			if (client.player != null) {
 
 				// Update player
 				player.update(client);
 
-				if (client.options.attackKey.isPressed() && !wasAttacking) {
-					player.attack(client);
+				// If event is happening
+				if (client.options.attackKey.isPressed()) {
+					player.whileAttack(client);
+					if (!wasAttacking) {
+						player.startAttack(client);
+					}
 				}
-				if (client.options.useKey.isPressed() && !wasUsing) {
-					player.use(client);
+				if (client.options.useKey.isPressed()) {
+					player.whileUse(client);
+					if (!wasUsing) {
+						player.startUse(client);
+					}
 				}
-				if (openContainerKey.isPressed() && !wasOpeningContainer) {
-					player.openContainer(client);
+				if (openContainerKey.isPressed()) {
+					player.whileOpenContainer(client);
+					if (!wasOpeningContainer) {
+						player.startOpenContainer(client);
+					}
 				}
-			}
+				if (client.player.isFallFlying()) {
+					player.whileFlying(client);
+					if (!wasFlying) {
+						player.startFlying(client);
+					}
+				}
 
-			// Update key states
-			wasAttacking = client.options.attackKey.isPressed();
-			wasUsing = client.options.useKey.isPressed();
-			wasOpeningContainer = openContainerKey.isPressed();
+				// If key stopped
+				if (!client.options.attackKey.isPressed() && wasAttacking) {
+					player.endAttack(client);
+				}
+				if (!client.options.useKey.isPressed() && wasUsing) {
+					player.endUse(client);
+				}
+				if (!openContainerKey.isPressed() && wasOpeningContainer) {
+					player.endOpenContainer(client);
+				}
+				if (!client.player.isFallFlying() && wasFlying) {
+					player.endFlying(client);
+				}
+
+				// Update key states
+				wasAttacking = client.options.attackKey.isPressed();
+				wasUsing = client.options.useKey.isPressed();
+				wasOpeningContainer = openContainerKey.isPressed();
+				wasFlying = client.player.isFallFlying();
+			}
 
 		});
 
--- src/client/java/com/github/utilcubed/autoinventory/Player.java
+++ src/client/java/com/github/utilcubed/autoinventory/Player.java
@@ -28,6 +28,9 @@ public class Player {
     private int firstUtilitySlot = 7;
     private int secondUtilitySlot = 8;
     private int enderChestSlot = -1;
+    private int shieldSlot = -1;
+    private int bowSlot = -1;
+    private int foodSlot = -1;
 
     private int openContainer_phase = 0;
     private int openContainer_y = 0;
@@ -39,38 +42,171 @@ public class Player {
     }
 
     // Left click
-    public void attack(MinecraftClient client) {
+    public void startAttack(MinecraftClient client) {
+
+    }
+
+    public void whileAttack(MinecraftClient client) {
+
+    }
+
+    public void endAttack(MinecraftClient client) {
 
     }
 
     // Right click
-    public void use(MinecraftClient client) {
+    public void startUse(MinecraftClient client) {
+        PlayerEntity player = client.player;
+        if (player == null || player.getInventory() == null || client.currentScreen != null)
+            return;
+
+        int playerSlot = player.getInventory().selectedSlot;
+
+        // Weapon slot
+        if (playerSlot == weaponSlot && getItemInSlot(client, PlayerInventory.OFF_HAND_SLOT) != Items.SHIELD) {
+            this.shieldSlot = findItemAndSwapTo(client, Items.SHIELD, PlayerInventory.OFF_HAND_SLOT);
+        }
+        // Tool slot
+        else if (playerSlot == toolSlot && getItemInSlot(client, PlayerInventory.OFF_HAND_SLOT) != Items.BOW) {
+            this.bowSlot = findItemAndSwapTo(client, Items.BOW, playerSlot);
+        }
+        // First utility slot
+        else if (playerSlot == firstUtilitySlot) {
+            BlockState targetedBlock = targetedBlockState(client, 6);
+            // No need to check null
+
+            // If air
+            if (targetedBlock == null || targetedBlock.isAir()) {
+                client.options.useKey.setPressed(false);
+                findItemAndSwapTo(client, Items.ENDER_PEARL, firstUtilitySlot);
+            }
+            // If scaffold
+            else if (targetedBlock.getBlock() == Blocks.SCAFFOLDING) {
+                client.options.useKey.setPressed(false);
+                findItemAndSwapTo(client, Items.SCAFFOLDING, firstUtilitySlot);
+            }
+
+        }
+        // Second utility slot
+        else if (playerSlot == secondUtilitySlot) {
+            BlockState targetedBlock = targetedBlockState(client, 6);
+            if (targetedBlock == null)
+                return;
+
+            // If solid
+            if (targetedBlock != null || !targetedBlock.isAir() && targetedBlock.isSolid()) {
+                client.options.useKey.setPressed(false);
+                findItemAndSwapTo(client, Items.TORCH, secondUtilitySlot);
+            }
+
+        }
+        // Anything else
+        else {
+            // If air
+            if (targetedBlockState(client, 6) == null || targetedBlockState(client, 6).isAir()) {
+                int foodSlot = getSlotOfItem(client, Items.GOLDEN_CARROT);
+                if (foodSlot == -1)
+                    getSlotOfItem(client, Items.COOKED_BEEF);
+                if (foodSlot == -1)
+                    getSlotOfItem(client, Items.COOKED_PORKCHOP);
+                if (foodSlot == -1)
+                    return;
+                swap(client, foodSlot, PlayerInventory.OFF_HAND_SLOT);
+                this.foodSlot = foodSlot;
+            }
+        }
 
     }
 
-    // Open container key
-    public void openContainer(MinecraftClient client) {
+    public void whileUse(MinecraftClient client) {
+        
+    }
 
+    public void endUse(MinecraftClient client) {
+        PlayerEntity player = client.player;
+
+        if (player == null || player.getInventory() == null || client.currentScreen != null)
+            return;
+
+        Item itemInOffhand = player.getOffHandStack().getItem();
+        Item itemInMainhand = player.getMainHandStack().getItem();
+
+        int playerSlot = player.getInventory().selectedSlot;
+        if (playerSlot == weaponSlot && itemInOffhand == Items.SHIELD) {
+            if (this.shieldSlot == -1)
+                return;
+
+            swap(client, shieldSlot, PlayerInventory.OFF_HAND_SLOT);
+            this.shieldSlot = -1;
+        } else if (playerSlot == toolSlot && itemInMainhand == Items.BOW) {
+            if (this.bowSlot == -1)
+                return;
+
+            swap(client, bowSlot, playerSlot);
+            this.bowSlot = -1;
+        }
+        else if (itemInOffhand == Items.GOLDEN_CARROT || itemInOffhand == Items.COOKED_BEEF || itemInOffhand == Items.COOKED_PORKCHOP) {
+            if (this.foodSlot == -1)
+                return;
+
+            swap(client, foodSlot, PlayerInventory.OFF_HAND_SLOT);
+            this.foodSlot = -1;
+        }
+    }
+
+    // Flying
+    public void startFlying(MinecraftClient client) {
+        if (client.player == null || client.player == null || client.currentScreen != null)
+            return;
+
+        findItemAndSwapTo(client, Items.FIREWORK_ROCKET, secondUtilitySlot);
+    }
+
+    public void whileFlying(MinecraftClient client) {
+
+    }
+
+    public void endFlying(MinecraftClient client) {
+        if (client.player == null || client.player == null || client.currentScreen != null)
+            return;
+
+        findItemAndSwapTo(client, Items.TORCH, secondUtilitySlot);
+    }
+
+    // Open container key
+    public void startOpenContainer(MinecraftClient client) {
         if (client.player == null || client.player.getInventory() == null)
             return;
 
-        int enderChestSlot = getItemSlot(client, Items.ENDER_CHEST);
+        if (client.currentScreen == null) {
+            int enderChestSlot = getSlotOfItem(client, Items.ENDER_CHEST);
 
-        // If no ender chest, end
-        if (enderChestSlot == -1)
-            return;
-        this.enderChestSlot = enderChestSlot;
-        swap(client, enderChestSlot, secondUtilitySlot);
-        client.player.getInventory().selectedSlot = secondUtilitySlot;
+            // If no ender chest, end
+            if (enderChestSlot == -1)
+                return;
+            this.enderChestSlot = enderChestSlot;
+            swap(client, enderChestSlot, secondUtilitySlot);
+            client.player.getInventory().selectedSlot = secondUtilitySlot;
 
-        // If not on ground, end
-        if (client.player.isOnGround() != true)
-            return;
-        client.player.setPitch(90.0f);
-        client.player.jump();
+            // If not on ground, end
+            if (client.player.isOnGround() != true)
+                return;
+            client.player.setPitch(90.0f);
+            client.player.jump();
 
-        openContainer_phase = 5;
-        openContainer_y = client.player.getBlockPos().getY();
+            openContainer_phase = 5;
+            openContainer_y = client.player.getBlockPos().getY();
+        } else if (client.currentScreen != null) {
+            // Future code
+        }
+    }
+
+    public void whileOpenContainer(MinecraftClient client) {
+
+    }
+
+    public void endOpenContainer(MinecraftClient client) {
+
     }
 
     public void openContainerUpdate(MinecraftClient client) {
@@ -78,7 +214,7 @@ public class Player {
             case 5:
                 // Swap silk touch pickaxe to toolSlot
                 openContainer_phase--;
-                int silkTouchSlot = getItemSlot(client, Items.DIAMOND_PICKAXE);
+                int silkTouchSlot = getSlotOfItem(client, Items.DIAMOND_PICKAXE);
                 if (silkTouchSlot == -1)
                     return;
                 swap(client, silkTouchSlot, toolSlot);
@@ -100,7 +236,8 @@ public class Player {
                 client.player.getInventory().selectedSlot = toolSlot;
             case 1:
                 // Open ender chest
-                if (targetedBlockState(client, 3).getBlock() != Blocks.ENDER_CHEST)
+                BlockState targetedBlock = targetedBlockState(client, 3);
+                if (targetedBlock == null || targetedBlock.getBlock() != Blocks.ENDER_CHEST)
                     return;
                 openContainer_phase--;
                 interact(client, Hand.MAIN_HAND, 3);
@@ -111,7 +248,7 @@ public class Player {
         }
     }
 
-    public int getItemSlot(MinecraftClient client, Item item) {
+    public int getSlotOfItem(MinecraftClient client, Item item) {
         int slot = -1;
         PlayerInventory inventory = client.player.getInventory();
         for (int i = 0; i < inventory.size(); i++) {
@@ -123,7 +260,26 @@ public class Player {
         return slot;
     }
 
+    public Item getItemInSlot(MinecraftClient client, int slot) {
+        PlayerInventory inventory = client.player.getInventory();
+        return inventory.getStack(slot).getItem();
+    }
+
+    public int findItemAndSwapTo(MinecraftClient client, Item item, int slot) {
+        int itemSlot = getSlotOfItem(client, item);
+        if (itemSlot == -1)
+            return -1;
+        if (itemSlot == client.player.getInventory().selectedSlot)
+            return itemSlot;
+
+        swap(client, itemSlot, slot);
+        return itemSlot;
+    }
+
     public void swap(MinecraftClient client, int from, int to) {
+        if (from == to)
+            return;
+
         ClientPlayerInteractionManager interactionManager = client.interactionManager;
 
         if (interactionManager != null && client.player != null && client.player.getInventory() != null) {
